aad0af64d19397836240e46967a684713bacdb22
giancarloz <gzabaleta48@gmail.com>
2020-07-17 09:05:55 -0400

    second attempt

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
   resources :elements
   resources :spots
   resources :users
   post '/login' => 'auth#login'
   get '/auth' => 'auth#persist'
+
   # get '*path', to: "application#fallback_index_html", constraints: ->(request) do
   #   !request.xhr? && request.format.html?
   # end
